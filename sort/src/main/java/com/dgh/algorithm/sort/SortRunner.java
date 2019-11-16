79a898c3c0c41bc26e3362efdf77af7e02602ac4
dingguohang <dingguohang@kuaishou.com>
2019-11-16 16:22:01 +0800

    change SortRunner

--- sort/src/main/java/com/dgh/algorithm/sort/SortRunner.java
+++ sort/src/main/java/com/dgh/algorithm/sort/SortRunner.java
@@ -2,7 +2,6 @@ package com.dgh.algorithm.sort;
 
 import com.google.common.base.Stopwatch;
 
-import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
@@ -17,19 +16,19 @@ public class SortRunner {
         int count = 10000;
         int[] input = new int[count];
         for (int i = 0; i < count; i++) {
-            input[i] = ThreadLocalRandom.current().nextInt(100);
+            input[i] = ThreadLocalRandom.current().nextInt(count);
         }
 
         Stopwatch watch = Stopwatch.createStarted();
         sorter.sort(input);
         watch.stop();
-
         isSorted(input);
-        System.out.println(Arrays.toString(input));
-        System.out.println(watch.elapsed(TimeUnit.MILLISECONDS));
+        System.out.println(String.format("Sorter: %s. Cost: %s", sorter.getClass().getSimpleName(),
+                watch.elapsed(TimeUnit.MILLISECONDS)));
     }
 
     public static void main(String[] args) {
         run(new InsertionSort());
+        run(new SelectionSort());
     }
 }
